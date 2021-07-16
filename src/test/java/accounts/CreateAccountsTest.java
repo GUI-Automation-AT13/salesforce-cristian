feec883938dfe57cc3836ead5b5d6c17604c8a88
cristianfchq <cristian.fchq@gmail.com>
2021-07-16 00:20:36 -0400

    solve issues

--- src/test/java/accounts/CreateAccountsTest.java
+++ src/test/java/accounts/CreateAccountsTest.java
@@ -1,10 +1,10 @@
 package accounts;
 
+import static core.config.Configuration.dotenv;
+
 import base.BaseTest;
 import org.testng.annotations.Test;
 import salesforce.ui.pages.HomePage;
-
-import static core.config.Configuration.dotenv;
 
 public class CreateAccountsTest extends BaseTest {
 
